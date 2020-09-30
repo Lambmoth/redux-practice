9cac803de0e929d7ff947a098914f107b33725a5
gjlandrews <gja@ualberta.ca>
2020-09-29 20:28:37 -0600

    added styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from './actions';
-
+import './App.css';
 
 
 function App() {
@@ -10,15 +10,18 @@ function App() {
   const dispatch = useDispatch();
    
   return (
-    <div className="App">
-      <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment(5))}>+5</button>
-      <button onClick={() => dispatch(increment(1))}>+</button>
-      <button onClick={() => dispatch(decrement(1))}>-</button>
-      <button onClick={() => dispatch(decrement(5))}>-5</button>
-
-      {isLogged ? <h3> Valuable Information users should see</h3> : ''} 
-      
+    <div className="app">
+      <div className="app">
+        <i className='pleaseIJustNeedAnOKJob'>Behold an incredible, job offer worthy example of Redux:</i>
+        <h1 className={counter>0 ? 'positive' : 'negative'}>{counter}</h1>
+      </div>
+      <div className="item">
+        <button className="minus" onClick={() => dispatch(decrement(5))}>-5</button>
+        <button className="minus" onClick={() => dispatch(decrement(1))}>-</button>
+        <button className="plus" onClick={() => dispatch(increment(1))}>+</button>
+        <button className="plus" onClick={() => dispatch(increment(5))}>+5</button>
+      </div>
+        {isLogged ? <div className='app'><h3> Valuable Information users should see</h3></div> : ''} 
     </div>
   );
 }
